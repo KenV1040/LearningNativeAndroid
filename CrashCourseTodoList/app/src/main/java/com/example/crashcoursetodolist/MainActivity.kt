634cb91226eac4d21c37f92d6c1136dc8e57b919
Ken <devworks.la5na@simplelogin.co>
2021-12-30 18:07:38 +0000

    Updated MainActivity to assign recycle view with the new items and more

--- CrashCourseTodoList/app/src/main/java/com/example/crashcoursetodolist/MainActivity.kt
+++ CrashCourseTodoList/app/src/main/java/com/example/crashcoursetodolist/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.crashcoursetodolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    // lateinit is a promise that we will initialise this variable later.
+    private lateinit var todoAdapter: TodoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        todoAdapter = TodoAdapter(mutableListOf())
+
+        rvTodoItems.adapter = todoAdapter
+        rvTodoItems.layoutManager = LinearLayoutManager(this)
+
+        btnAddTodo.setOnClickListener {
+            val todoTitle = etTodoTitle.text.toString()
+            if (todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                etTodoTitle.text.clear()
+            }
+        }
+
+        btnDeleteDoneTodos.setOnClickListener {
+            todoAdapter.deleteDoneTodo()
+        }
     }
 }
